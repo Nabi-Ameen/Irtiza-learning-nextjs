b248c1c95c2969e601e3ada2d15ced434f95bee1
Nabi-Ameen <nabiameen35126@gmail.com>
2026-02-18 09:42:35 +0500

    set multiple checkboxes

--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,29 @@ export default function Home() {
     Fname: "",
     Lname: "",
     city: "",
+    gender: "",
+    tours: {
+      cityTour: false,
+      museumTour: false,
+      foodTour: false,
+    },
   });
 
+  console.log("form", form);
   const handleSubmit = () => {
     console.log("form", form);
+  };
+
+  const handleCheckbox = (e: any) => {
+    const { name, checked } = e.target;
+
+    setForm({
+      ...form,
+      tours: {
+        ...form.tours,
+        [name]: checked,
+      },
+    });
   };
 
   return (
@@ -52,6 +71,70 @@ export default function Home() {
               );
             })}
           </select>
+
+          <div>
+            <label htmlFor="Male">
+              <input
+                type="radio"
+                id="Male"
+                name="gender"
+                value="Male"
+                checked={form?.gender === "Male"}
+                onChange={(e) => setForm({ ...form, gender: e.target.value })}
+              />
+              Male
+            </label>
+
+            <label className="ml-2" htmlFor="Female">
+              <input
+                type="radio"
+                id="Female"
+                className="ml-10"
+                name="gender"
+                value="Female"
+                checked={form?.gender === "Female"}
+                onChange={(e) => {
+                  setForm({ ...form, gender: e.target.value });
+                }}
+              />
+              Female
+            </label>
+          </div>
+
+          <div>
+            <label className="ml-2" htmlFor="Female">
+              <input
+                type="checkbox"
+                name="cityTour"
+                className="mx-3 mt-1"
+                checked={form?.tours.cityTour}
+                onChange={handleCheckbox}
+              />
+              cityTour
+            </label>
+
+            <label className="ml-2" htmlFor="Female">
+              <input
+                type="checkbox"
+                name="museumTour"
+                className="mx-3 mt-1"
+                checked={form?.tours.museumTour}
+                onChange={handleCheckbox}
+              />
+              museumTour
+            </label>
+
+            <label className="ml-2" htmlFor="Female">
+              <input
+                type="checkbox"
+                name="foodTour"
+                className="mx-3 mt-1"
+                checked={form?.tours.foodTour}
+                onChange={handleCheckbox}
+              />
+              foodTour
+            </label>
+          </div>
 
           <button
             className="bg-blue-800 text-white rounded-lg p-4"
